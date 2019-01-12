bc8154164f7035313b5f6b1902d09e9a83343769
Shelly Branch <sbranch@homeaway.com>
2019-01-12 21:23:36 +0000

    Updated the file with the function that has a for each with a callback

--- callbackAruments.js
+++ callbackAruments.js
@@ -1,17 +1,34 @@
 
-var person = '';
+
 function findWaldo(arr, found) {
   for (var i = 0; i < arr.length; i++) {
     
     if (arr[i] === "Waldo") {
-      person = i;
-      found();   // execute callback
+      
+      found(i);   // execute callback
     }
   }
 }
 
-function actionWhenFound() {
-  console.log("Found Waldo at index " + person);
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+//ForEach
+
+function findWaldo(arr, found) {
+  arr.forEach(function(element, index) {
+  if (element === "Waldo") {
+    found(index);
+  }
+});
+ 
+}
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
