9c8cdd298c82e8379abcc105ae2ae2f0c4c24d79
Shelly Branch <sbranch@homeaway.com>
2019-01-12 22:36:48 +0000

    added the map callback function to the file

--- callbackAruments.js
+++ callbackAruments.js
@@ -32,3 +32,33 @@ function actionWhenFound(index) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+// Using map for a Callback
+
+//map over input array
+//target x and y values and math formula 
+
+
+
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var total;
+var a;
+var b;
+var result = input.map(obj => {
+a = obj.x ** 2;
+b = obj.y ** 2;
+total = a + b
+console.log(Math.sqrt(total))
+});
+
+
+//const map1 = array1.map(x => x * 2);
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
