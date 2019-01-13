d41f1b48592ff6c74e9d11873ffc3fd77646323b
Shelly Branch <sbranch@homeaway.com>
2019-01-13 01:49:24 +0000

    Added the map assignment to the file

--- callbackAruments.js
+++ callbackAruments.js
@@ -112,3 +112,28 @@ console.log(students)
 
 }
 sortStudents()
+
+
+//Addig the Map Assignment
+
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
+
+function map(arr, callback) {
+  var newArr = []
+  for (var i = 0; i < arr.length; i++) {
+    newArr.push(callback(arr[i]));
+  } 
+  console.log(newArr)
+}
