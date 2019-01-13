83f406c0659e031d0aa69c704b5da12a4eead1f9
Shelly Branch <sbranch@homeaway.com>
2019-01-13 00:00:49 +0000

    Add Sorting by key values in an object to the file

--- callbackAruments.js
+++ callbackAruments.js
@@ -62,3 +62,53 @@ console.log(Math.sqrt(total))
 console.log(result[0] === 5);
 console.log(result[1] === 13);
 console.log(result[2] === 17);
+
+// Sorting by key values in an object
+
+var array = [10, 2, 5, 1, 9 ];
+array.sort(function(a,b){
+  return a - b;
+});
+var array = ['c', 'a', 'b', 'e', 'd' ];
+array.sort();
+
+//Say we have a list of students that we would like to sort in alphabetical order. If students have the same name, then we prioritize the older student first.
+
+// write a function that will check to see if the names match.
+// If they match, use the sort callback function on the age to list the students
+// else do a general sort
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+var result = {};
+function sortStudents(){
+// sort by value
+students.sort(function (a, b) {
+  return b.age - a.age;
+});
+//console.log(students)
+
+// sort by name
+students.sort(function(a, b) {
+  var nameA = a.name; 
+  var nameB = b.name; 
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  if(nameA === nameB){
+    return 0;
+  }
+
+  // names must be equal
+  //return 0;
+});
+console.log(students)
+
+}
+sortStudents()
